11eba982fce8e22b5a19ea69cd7064cd1247dbbe
mj <wmj0924@gmail.com>
2024-05-21 14:20:52 +0900

    글목록

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./page/Home.jsx";
+import { BoardWrite } from "./page/board/BoardWrite.jsx";
+import { BoardList } from "./page/board/BoardList.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        index: true,
+        element: <BoardList />,
+      },
+      { path: "write", element: <BoardWrite /> },
+    ],
+  },
+]);
 
 function App(props) {
   return (
-    <div></div>
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   );
 }
 
 export default App;
-
