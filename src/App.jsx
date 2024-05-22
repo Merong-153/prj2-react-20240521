8f5be9789ba05c59f295b71e2b575885d90d02ff
mj <wmj0924@gmail.com>
2024-05-22 12:37:05 +0900

    20240522

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./page/Home.jsx";
 import { BoardWrite } from "./page/board/BoardWrite.jsx";
 import { BoardList } from "./page/board/BoardList.jsx";
+import { BoardView } from "./page/board/BoardView.jsx";
+import { BoardEdit } from "./page/board/BoardEdit.jsx";
+import { MemberSignup } from "./page/member/MemberSignup.jsx";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +18,9 @@ const router = createBrowserRouter([
         element: <BoardList />,
       },
       { path: "write", element: <BoardWrite /> },
+      { path: "board/:id", element: <BoardView /> },
+      { path: "edit/:id", element: <BoardEdit /> },
+      { path: "signup", element: <MemberSignup /> },
     ],
   },
 ]);
